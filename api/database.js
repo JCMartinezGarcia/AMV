bb83845d5592288674827dc9da5c6e7b1db1278e
julio <jcmtz.091@gmail.com>
2025-08-20 01:47:36 -0600

    add creation of tables

--- api/database.js
+++ api/database.js
@@ -4,11 +4,18 @@ const db = new sqlite3.Database(':memory:', (err) => {
         console.error('Error connecting to database:', err.message);
     } else {
         console.log('Connected to the SQLite database.');
-        // db.run(`CREATE TABLE IF NOT EXISTS users (
-        //     id INTEGER PRIMARY KEY AUTOINCREMENT,
-        //     name TEXT NOT NULL,
-        //     email TEXT UNIQUE NOT NULL
-        // )`);
+        db.run(`CREATE TABLE IF NOT EXISTS clients (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            age INTEGER NOT NULL,
+            symptoms TEXT NOT NULL
+        )`, (err) => {
+            if (err) {
+                console.error('Error creating database tables:', err.message);
+            }else{
+                console.log('Database tables created successfully.');
+            }
+        });
     }
 });
 
