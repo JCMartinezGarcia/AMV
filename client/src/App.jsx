0be5ea759ce51c863132d7e9064fa890b5dafd92
julio <jcmtz.091@gmail.com>
2025-08-26 23:31:31 -0600

    feat(patients): adds patient FE paginated table

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Patients from "./pages/Patients";
 function App() {
 
   return (
@@ -7,6 +8,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/patients" element={<Patients />} />
         </Routes>
       </Router>
     </>
